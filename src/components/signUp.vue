4c304a8cdbb6049588760626abf6ca69ad285962
fanxiaolong <fanxiaolongfe@gmail.com>
2017-03-30 18:20:15 +0800

    添加注册页面

--- src/components/signUp.vue
+++ src/components/signUp.vue
@@ -8,16 +8,14 @@
 </style>
 <template>
   <div class="signin">
-    这是登录页面
+    这是注册页面
     <input type="text" placeholder="请输入用户名" v-model="name">
     <input type="password" placeholder="请输入密码" v-model="password">
-    <el-button @click="login" type="primary">登录</el-button>
+    <el-button @click="login" type="primary">注册</el-button>
   </div>
 </template>
 
 <script>
-  import {SIGN_IN} from '@/store/mutation-types'
-
   export default {
     data(){
       return {
@@ -28,21 +26,16 @@
     },
     methods:{
       login() {
-        this.$http.post('http://localhost:3000/api/signin',{
+        this.$http.post('http://localhost:3000/api/signup',{
           // params:{
             name:this.name,
             password:this.password
           // }
         }).then((res)=>{
           if (res.data.code===200) {
-            this.$store.commit(SIGN_IN)
-            if(this.targetUrl){
-              this.$router.replace(this.targetUrl)
-            }else{
-              this.$router.replace({
-                name: 'Index'
-              })
-            }
+            this.$router.push({
+              name:'Index'
+            })
           }
         })
       }
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import usersLogin from '@/components/usersLogin'
+import signUp from '@/components/signUp'
 import adminIndex from '@/components/adminIndex'
 
 Vue.use(Router)
@@ -14,6 +15,10 @@ let routes = [{
   path: '/login',
   name: 'usersLogin',
   component: usersLogin
+},{
+  path: '/signUp',
+  name: 'signUp',
+  component: signUp
 },{
   path: '/admin',
   name: 'adminIndex',
